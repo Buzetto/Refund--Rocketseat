9c7252d71771064392716cd1b581b69f74ec311e
Victor Buzetto <victoraugustobuzetto@gmail.com>
2025-11-23 10:20:11 -0300

    Criando a função para o valor ser digitado na moeda Real Brasileiro

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
 
+// Capturando o evento de input do valor para formatar somente em numero
 amount.oninput = () => {
+    //Obtem o valor atual do input e remove os caracteres não numericos
     let value = amount.value.replace(/\D/g, "");
-    amount.value = value;
+
+    //Transformar o valor em centavos (Exemplo: 150/100 = 1.50)
+    value = Number(value) / 100;
+
+    //Atualiza o valor do input
+    amount.value = formtaCurrencyBRL(value);
+}
+
+//Função que converte o valor do input de valor para moeda local que é Real Brasileiro
+function formtaCurrencyBRL (value) {
+    //Formata no padrão BRL
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    //Retorna o valor formatado
+    return value;
 }
