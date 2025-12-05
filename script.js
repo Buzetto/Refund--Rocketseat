0067cd27b154ad77f298216d50489b43ad334ea5
Victor Buzetto <victoraugustobuzetto@gmail.com>
2025-12-05 14:25:24 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -96,6 +96,9 @@ function expenseAdd(newExpense) {
         //Adiciona o item na lista
         expenseList.append(expenseItem, expenseInfo, expenseAmount, removeIcon);
 
+        //Limpa o formulário para adicionar um novo item
+        formClear();
+
         //Atualiza os totais
         updateTotals();
 
@@ -166,3 +169,11 @@ expenseList.addEventListener("click", function (event) {
     //Atualiza os totais
     updateTotals();
 })
+
+function formClear () {
+    expense.value = "";
+    category.value = "";
+    amount.value = "";
+
+    expense.focus();
+}
